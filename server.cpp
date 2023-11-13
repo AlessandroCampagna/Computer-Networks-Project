0b357e5c64e87cc191af2fea311bedc7b5c2003e
Corte <josesantocorte@gmail.com>
2023-11-13 16:34:21 +0000

    Server

--- server.cpp
+++ server.cpp
@@ -1,5 +1,33 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdio>
 
-int main(int argc, char *argv[]){
-    printf("Hello World \n");
+int main(int argc, char *argv[]) {
+    int ASport = -1;
+    int GN = 17;
+    bool Verbose = false;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "n:p:")) != -1) {
+        switch (opt) {
+        case 'p':
+            ASport = atoi(optarg);
+            break;
+        case 'v':
+            Verbose = true;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-n ASIP] [-p ASport]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Default settings
+    if (ASport == -1) ASport = 58000 + GN; // default port
+    if (Verbose) printf("ASport = %d\n", ASport);
+
+    // rest of your code
+
+    return 0;
 }
