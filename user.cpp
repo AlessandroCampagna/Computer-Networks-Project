976efc4cf4bb4ad590cec86a7e02159c62a16629
Alessandro Campagna <alecostacamp@gmail.com>
2023-11-14 12:44:54 +0000

    user

--- user.cpp
+++ user.cpp
@@ -1,12 +1,30 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <string.h>
+#include <stdio.h>
 #include <cstdio>
+
+#DEFINE PORT "58017"
+#define BUFFER_SIZE 128
 
 int main(int argc, char *argv[]) {
     char *ASIP = NULL;
     int ASport = -1;
-    int GN = 17;
+    int uid;
+    int aid;
+    bool logged = false;
+
+    int fd,errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    struct addrinfo hints,*res;
+    struct sockaddr_in addr;
+    char buffer[BUFFER_SIZE];
 
     int opt;
     while ((opt = getopt(argc, argv, "n:p:")) != -1) {
@@ -24,12 +42,33 @@ int main(int argc, char *argv[]) {
     }
 
     // Default settings
-    if (ASIP == NULL) ASIP = strdup("localhost"); // default IP
-    if (ASport == -1) ASport = 58000 + GN; // default port
+    if (ASIP == NULL) ASIP = strdup("localhost");            // default IP
+    if (ASport == -1) ASport = PORT;                        // default port
 
-    // rest of your code
-    printf("ASIP: %s\n", ASIP);
-    printf("ASport: %d\n", ASport);
+    fd=socket(AF_INET,SOCK_DGRAM,0);                      //UDP socket
+    if(fd==-1) /*error*/exit(1)
+
+    memset(&hints,0,sizeof hints);
+    hints.ai_family=AF_INET;                          //IPv4
+    hints.ai_socktype=SOCK_DGRAM;                    //UDP socket
+
+    errcode=getaddrinfo(ASIP,ASport,&hints,&res);
+    if(errcode!=0) /*error*/exit(1);
+
+    fgets(buffer, sizeof(buffer), stdin);
+
+    n=sendto(fd,buffer,strlen(buffer),0,res->ai_addr,res->ai_addrlen);
+    if(n==-1) /*error*/ exit(1);
+
+    memset(&buffer,0,sizeof(buffer));
+    addrlen=sizeof(addr);
+    n=recvfrom(fd,buffer,BUFFER_SIZE,0,(struct sockaddr*)&addr,&addrlen);
+    if(n==-1) /*error*/ exit(1);
+
+    write(1,"echo: ",6); write(1,buffer,n);
+
+
+    
 
 
     free(ASIP);
