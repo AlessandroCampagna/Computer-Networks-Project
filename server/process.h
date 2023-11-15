502f388a90be80e32a7d658597d78c13e36ec382
Corte <josesantocorte@gmail.com>
2023-11-15 00:18:38 +0000

    Remove gitignore stupid ale coments

---
+++ server/process.h
@@ -0,0 +1,11 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <vector>
+#include <sstream>
+#include <string>
+#include <cstdio>
+
+int process_request(char *buffer);
+
+#endif
---
+++ server/server.h
@@ -0,0 +1,22 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <cstdio>
+#include <string>
+
+#define BUFFER_SIZE 128
+#define PORT 58000
+#define GROUP_NUMBER 17
+
+int process_request(char *buffer);
+
+#endif
