98b8f929e17407c324997b28134659b02e55f0e6
Alessandro Campagna <alecostacamp@gmail.com>
2023-11-15 22:38:46 +0000

    beggining of working on functions

--- src/user/user.cpp
+++ src/user/user.cpp
@@ -1,16 +1,4 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <string.h>
-#include <stdio.h>
-#include <cstdio>
-
-#define PORT 58000
-#define BUFFER_SIZE 128
+#include "user.h"
 
 int main(int argc, char *argv[]) {
     char *ASIP = NULL;
@@ -21,10 +9,10 @@ int main(int argc, char *argv[]) {
     int aid;
     bool logged = false;
 
-    int fd,errcode;
+    int fd_udp, fd_tcp;
     ssize_t n;
     socklen_t addrlen;
-    struct addrinfo hints,*res;
+    struct addrinfo hints_udp, hints_tcp, *res_udp, *res_tcp;
     struct sockaddr_in addr;
     char buffer[BUFFER_SIZE];
 
@@ -48,31 +36,41 @@ int main(int argc, char *argv[]) {
     if (ASport == -1) ASport = PORT + GN; // default port
     sprintf(ASportStr, "%d", ASport); // convert port to string
 
-    fd=socket(AF_INET,SOCK_DGRAM,0); //UDP socket
-    if(fd==-1) /*error*/exit(1);
+    fd_udp=socket(AF_INET,SOCK_DGRAM,0); //UDP socket
+    fd=socket(AF_INET,SOCK_STREAM,0); //TCP socket
 
-    memset(&hints,0,sizeof hints);
-    hints.ai_family=AF_INET; //IPv4
-    hints.ai_socktype=SOCK_DGRAM; //UDP socket
+    memset(&hints_udp,0,sizeof hints_udp);
+    hints_udp.ai_family=AF_INET; //IPv4
+    hints_udp.ai_socktype=SOCK_DGRAM; //UDP socket
 
-    errcode=getaddrinfo(ASIP,ASportStr,&hints,&res);
-    if(errcode!=0) /*error*/exit(1);
+    memset(&hints_tcp,0,sizeof hints_tcp);
+    hints_tcp.ai_family=AF_INET; //IPv4
+    hints_tcp.ai_socktype=SOCK_STREAM; //TCP socket
 
-    fgets(buffer, sizeof(buffer), stdin);
+    getaddrinfo(ASIP,ASportStr,&hints_udp,&res_udp);
+    getaddrinfo(ASIP,ASportStr,&hints_tcp,&res_tcp)
 
-    printf("Sending message to server...\n");
-    n=sendto(fd,buffer,strlen(buffer),0,res->ai_addr,res->ai_addrlen);
-    if(n==-1) /*error*/ exit(1);
+    while(true){
 
-    printf("Awaiting response from server...\n");
-    memset(&buffer,0,sizeof(buffer));
-    addrlen=sizeof(addr);
-    n=recvfrom(fd,buffer,BUFFER_SIZE,0,(struct sockaddr*)&addr,&addrlen);
-    if(n==-1) /*error*/ exit(1);
+        fgets(buffer, sizeof(buffer), stdin);
 
-    write(1,"echo: ",6); write(1,buffer,n);
+        if()
+        n=sendto(fd_udp,buffer,strlen(buffer),0,res->ai_addr,res->ai_addrlen);
 
+        else if(res->ai_socktype==SOCK_STREAM)
+        n=connect(fd_udp,res->ai_addr,res->ai_addrlen);
+
+
+        memset(&buffer,0,sizeof(buffer));
+        addrlen=sizeof(addr);
+        n=recvfrom(fd_udp,buffer,BUFFER_SIZE,0,(struct sockaddr*)&addr,&addrlen);
+
+        write(1,"echo: ",6); write(1,buffer,n);
+
+    }
 
     free(ASIP);
     return 0;
 }
+
+
---
+++ src/user/user.h
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <cstdio>
+
+#define PORT 58000
+#define BUFFER_SIZE 128
+
+enum ConnectionType {
+    UDP,
+    TCP
+};
