e4919e81482d9f1f670f05ae3c2f7af803089d23
Corte <josesantocorte@gmail.com>
2023-11-17 09:21:03 +0000

    Fix ale code

--- src/user/handler.cpp
+++ src/user/handler.cpp
@@ -2,9 +2,6 @@
 
 using CommandFunction = std::function<ConnectionType(std::vector<std::string>*)>;
 
-std::vector<std::string> split_string(char*);
-void unsplit_string(char*, std::vector<std::string>*);
-
 ConnectionType login(std::vector<std::string>*);
 ConnectionType logout(std::vector<std::string>*);
 ConnectionType unregister(std::vector<std::string>*);
@@ -22,16 +19,33 @@ const std::unordered_map<std::string, CommandFunction> command_map = {
     {"exit", exituser}
 };
 
+std::vector<std::string> parse_buffer(char *buffer) {
+
+    // Make the buffer a cpp string
+    std::string str(buffer);
+    
+    // Split the string into tokens
+    std::string delimiter = " ";
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(str);
+
+    while (std::getline(tokenStream, token, delimiter[0])) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
 // Define the user_command function
 ConnectionType user_command(char* buffer) {
     
-    std::vector<std::string> buffer_words = split_string(buffer);
-    auto it = command_map.find(buffer_words[0]);
+    std::vector<std::string> tokens = parse_buffer(buffer);
+    auto it = command_map.find(tokens[0]);
 
     // If the command is found, execute the associated function and return its value
     if (it != command_map.end()) {
-        ConnectionType result = it->second(&buffer_words);
-        unsplit_string(buffer, &buffer_words);
+        ConnectionType result = it->second(&tokens);
         return result;
     }
 
@@ -39,59 +53,35 @@ ConnectionType user_command(char* buffer) {
     return ConnectionType::INVALID;
 }
 
-std::vector<std::string> split_string(char* buffer) {
-    std::string str(buffer);
-    std::vector<std::string> words;
-    std::string word = "";
-    for (auto c : str) {
-        if ((c == ' ') or (c == '\n')) {
-            words.push_back(word);
-            word = "";
-        } else {
-            word += c;
-        }
-    }
-    words.push_back(word);
-    return words;
-}
-
-void unsplit_string(char* buffer, std::vector<std::string>* words) {
-    std::string result = "";
-    for (auto word : *words) {
-        result += word + " ";
-    }
-    std::strcpy(buffer, result.c_str());
-}
-
 // Define the functions for each command
-ConnectionType login(std::vector<std::string>* buffer_str) {
-    (*buffer_str)[0] = "LIN";
-    uid = (*buffer_str)[1];
-    password = (*buffer_str)[2];
+ConnectionType login(std::vector<std::string>* tokens) {
+    (*tokens)[0] = "LIN";
+    std::string uid = (*tokens)[1];
+    std::string password = (*tokens)[2];
     return ConnectionType::UDP;
 }
 
-ConnectionType logout(std::vector<std::string>* buffer_str) {
-    (*buffer_str)[0] = "LOU";
-    buffer_str->push_back(uid);
-    buffer_str->push_back(password);
+ConnectionType logout(std::vector<std::string>* tokens) {
+    (*tokens)[0] = "LIN";
+    std::string uid = (*tokens)[1];
+    std::string password = (*tokens)[2];
     return ConnectionType::UDP;
 }
 
-ConnectionType unregister(std::vector<std::string>* buffer_str) {
-    (*buffer_str)[0] = "UNR";
-    buffer_str->push_back(uid);
-    buffer_str->push_back(password);
+ConnectionType unregister(std::vector<std::string>* tokens) {
+    (*tokens)[0] = "LIN";
+    std::string uid = (*tokens)[1];
+    std::string password = (*tokens)[2];
+    return ConnectionType::UDP;
+}
+ConnectionType myauctions(std::vector<std::string>* tokens) {
+    (*tokens)[0] = "LIN";
+    std::string uid = (*tokens)[1];
+    std::string password = (*tokens)[2];
     return ConnectionType::UDP;
 }
 
-ConnectionType myauctions(std::vector<std::string>* buffer_str) {
-    (*buffer_str)[0] = "LMA";
-    buffer_str->push_back(uid);
-    return ConnectionType::UDP;
-}
-
-ConnectionType exituser(std::vector<std::string>* buffer_str) {
+ConnectionType exituser(std::vector<std::string>* tokens) {
     return ConnectionType::EXIT;
 }
 
